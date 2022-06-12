62f4fe0f847828c77e26d7d7fc7ebf974053ea52
john <jkull247@gmail.com>
2022-06-12 15:31:12 -0700

    first push to github

---
+++ hello/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class schools(model, model):
+    name = models.CharField(max_length=23)
+    address = models.CharField(max_length=23)
+
+class country(model, model):
+    name = models.CharField(max_length=23)
+    
---
+++ hello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+class schools(model, model):
+    name = models.CharField(max_length=23)
+    address = models.CharField(max_length=23)
+
+class country(model, model):
+    name = models.CharField(max_length=23)
